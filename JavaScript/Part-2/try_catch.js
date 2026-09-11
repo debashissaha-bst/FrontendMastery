23f98235686bf01219442ee3ac731f8d22dbe7bb
Debashis Saha <debashissaha.bst@gmail.com>
2026-09-11 23:59:37 +0600

    js update1

---
+++ JavaScript/Part-2/try_catch.js
@@ -0,0 +1,8 @@
+try {
+    let result = 10 / 0;
+    console.log(result);
+
+    console.log(username);
+} catch (error) {
+    console.log("An error occurred here!");
+}
---
+++ JavaScript/Part-3/arrowfunction.js
@@ -0,0 +1,12 @@
+const add = (a, b) => {
+    return a + b;
+};
+
+console.log(add(10, 20));
+
+
+
+
+const add1 = (a, b) => a + b;
+
+console.log(add1(10, 20));
